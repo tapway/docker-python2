51f6de744a06985e87f3337c01be2e1cfac6a7d7
iaroslav <iarruss@ya.ru>
2018-05-14 19:51:06 +0100

    [add] remote tags viewer

---
+++ docker-tags.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# https://stackoverflow.com/a/39454426/4249707
+# don't forget about chmod +x docker-tags
+
+if [ $# -lt 1 ]
+then
+cat << HELP
+
+dockertags  --  list all tags for a Docker image on a remote registry.
+
+EXAMPLE:
+    - list all tags for ubuntu:
+       dockertags ubuntu
+
+    - list all php tags containing apache:
+       dockertags php apache
+
+    - list all tags of our repo
+       ./docker-tags.sh iarruss/docker-python2
+
+HELP
+fi
+
+image="$1"
+tags=`wget -q https://registry.hub.docker.com/v1/repositories/${image}/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}'`
+
+if [ -n "$2" ]
+then
+    tags=` echo "${tags}" | grep "$2" `
+fi
+
+echo "${tags}"
